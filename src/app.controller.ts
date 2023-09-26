f9da6900f4221e60d3fecae984492eeb3b335133
nguyen.quang.huy-e@sun-asterisk.com <nguyen.quang.huy-e@sun-asterisk.com>
2023-09-26 10:54:23 +0700

    web push

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,81 @@
-import { Controller, Get } from '@nestjs/common';
-import { AppService } from './app.service';
+import {
+  Body,
+  Controller,
+  Get,
+  NotFoundException,
+  Param,
+  Post,
+  Req,
+  Res,
+} from '@nestjs/common';
+import * as webPush from 'web-push';
+import { join } from 'path';
+import { readFileSync, existsSync, writeFileSync } from 'fs';
+import { v4 } from 'uuid';
+
+type device = {
+  endpoint: string;
+  keys: {
+    auth: string;
+    p256dh: string;
+  };
+};
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  // load client site
+  @Get('/*')
+  client(@Req() req, @Res() res): any {
+    try {
+      const filePath = join(__dirname, `../client/${req.url}`);
+      if (existsSync(filePath)) {
+        return res.sendFile(filePath);
+      }
 
-  @Get()
-  getHello(): string {
-    return this.appService.getHello();
+      throw new NotFoundException();
+    } catch (error) {
+      throw new NotFoundException();
+    }
+  }
+
+  // register device info
+  @Post('/register')
+  register(@Body() device: device): boolean {
+    try {
+      const filePath = join(__dirname, '../data/data.json');
+      const data = JSON.parse(readFileSync(filePath, 'utf-8'));
+      data.push({
+        userId: v4(),
+        device,
+      });
+
+      writeFileSync(filePath, JSON.stringify(data));
+
+      return true;
+    } catch (error) {
+      return error;
+    }
+  }
+
+  // push notify to browser
+  @Post('/subscribe/:userId')
+  test(@Param('userId') userId: string): boolean {
+    const filePath = join(__dirname, '../data/data.json');
+    const data = JSON.parse(readFileSync(filePath, 'utf-8'));
+    const user = data.find((item) => item.userId === userId);
+
+    if (!user) {
+      throw new NotFoundException();
+    }
+
+    const payload = JSON.stringify({
+      title: 'You have a message',
+    });
+
+    webPush
+      .sendNotification(user.device, payload)
+      .catch((error) => console.error(error));
+
+    return true;
   }
 }
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,17 @@
 import { NestFactory } from '@nestjs/core';
+import * as webPush from 'web-push';
+
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+
+  webPush.setVapidDetails(
+    process.env.SUBJECT,
+    process.env.PUBLIC_VAPID_KEY,
+    process.env.PRIVATE_VAPID_KEY,
+  );
+
+  await app.listen(process.env.PORT);
 }
 bootstrap();
